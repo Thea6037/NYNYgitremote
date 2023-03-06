52faec32ae6d5a6edec0123071410809c81e84ef
Thea6037 <thea6037@stud.kea.dk>
2023-03-06 14:11:04 +0100

    lala

---
+++ src/test/java/com/example/demoremote/DemoremoteApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demoremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoremoteApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
